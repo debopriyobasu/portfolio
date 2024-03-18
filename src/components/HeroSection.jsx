9c8a71f8f5a51dfaae0889cfa5859c6de458133a
Debopriyo Basu <debopriyobasu@gmail.com>
2024-03-18 22:31:39 +0530

    impr: updated heading font

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,11 +6,11 @@ const HeroSection = () => {
   return (
     <section id="hero" className="container">
       <div id="hero-text">
-        <h1 tabIndex={0}>
+        <h1 tabIndex={0} className="font-wix fw-bold">
           {greeting}!
           <br /> I am Debopriyo Basu
         </h1>
-        <h3 className="mt-2 m-mt-1" tabIndex={0}>
+        <h3 className="mt-2 m-mt-1 fw-normal" tabIndex={0}>
           A software engineer who builds beautiful and engaging web experiences
         </h3>
       </div>
